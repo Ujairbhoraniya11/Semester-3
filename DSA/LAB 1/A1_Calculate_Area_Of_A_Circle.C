8732551ec54807aa1a328f8c602cb745bac39623
MohammadUjair Bhoraniya <bhoraniyaujer@gmail.com>
2026-06-28 18:36:35 +0530

    Update A1_Calculate_Area_Of_A_Circle.C

--- DSA/LAB 1/A1_Calculate_Area_Of_A_Circle.C
+++ DSA/LAB 1/A1_Calculate_Area_Of_A_Circle.C
@@ -1,1 +1,17 @@
+// WAP to calculate area of a Circle (A = πr2).
 
+#include <stdio.h>
+
+int main() {
+    int r;
+    float pi = 22 / 7, ans;
+    
+    printf("Enter R : ");
+    scanf("%d",&r);
+    
+    ans = pi * (r * r);
+    
+    printf("Are of a Circle : %f", ans);
+    
+    return 0;
+}
